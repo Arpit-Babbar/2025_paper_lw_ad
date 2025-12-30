654b85a738fca8570378286ee1fd29fff64b6e1f
Arpit Babbar <arpitbabbar@gmail.com>
2025-12-30 16:14:56 +0530

    Clean up

--- generate_1d.jl
+++ generate_1d.jl
@@ -11,7 +11,6 @@ mkpath(tenkaiad_data_dir)
 
 isentropic_dir = joinpath(tenkaiad_data_dir, "isentropic_euler_double_rarefaction")
 isentropic_ref_dir = joinpath(tenkaiad_data_dir, "isentropic_euler_double_rarefaction_ref")
-#=
 trixi_include("examples/run_isentropic_euler_double_rarefaction.jl", nx = 200, saveto = isentropic_dir)
 
 trixi_include(joinpath("examples/run_isentropic_euler_double_rarefaction.jl"),
@@ -21,7 +20,6 @@ trixi_include(joinpath("examples/run_isentropic_euler_double_rarefaction.jl"),
                        bound_limit = "no",
                        limiter = Tenkai.setup_limiter_none(),
                        compute_error_interval = 0)
-=#
 files = [joinpath(isentropic_ref_dir, "sol.txt"), joinpath(isentropic_dir, "sol.txt")]
 labels = ["Reference", "LW3"]
 
